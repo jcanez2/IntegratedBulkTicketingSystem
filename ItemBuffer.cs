39068392aa4f954acb2b2ad14b61d0429284b659
jcanez2 <jcanez2@asu.edu>
2020-09-16 23:29:39 -0700

    Made Changes to Buffer to eliminate un-needed variables

---
+++ ItemBuffer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace IntegratedBulkTicketingSystem
+{
+    public class ItemBuffer
+    {
+        private const int MaxNumberOfOrders = 3;
+        private string[] orderBuffer;
+        private int _numberOfOrders = 0;
+        private static Semaphore _writeSemaphores; // pool of writing resources
+        private static Semaphore _readSemaphores; // pool of reading resources
+
+        public ItemBuffer()
+        {
+            lock (this)
+            {
+                try
+                {
+                    _writeSemaphores = new Semaphore(MaxNumberOfOrders, MaxNumberOfOrders);
+                    _readSemaphores = new Semaphore(MaxNumberOfOrders, MaxNumberOfOrders);
+                    orderBuffer = new string[MaxNumberOfOrders];
+                    InitiateBuffer();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e} \n Error occurred when constructing ItemBuffer" );
+                    throw;
+                }
+            }
+        }
+
+        private void InitiateBuffer()
+        {
+            for (int i = 0; i < MaxNumberOfOrders; i++)
+            {
+                orderBuffer[i] = "empty"; // set all values to "empty"
+            }
+        }
+
+        public string getOneCell()
+        {
+            _readSemaphores.WaitOne();
+            string retrievedOrder = "empty Order";
+
+            lock (this)
+            {
+                while (_numberOfOrders <= 0) // wait for an order to an order to appear
+                {
+                    Monitor.Wait(this);
+                }
+
+                for (int i = 0; i < MaxNumberOfOrders; i++)
+                {
+                    if (orderBuffer[i] != "empty") // make sure the order is not empty
+                    {
+                        retrievedOrder = orderBuffer[i];
+                        orderBuffer[i] = "empty";
+                        _numberOfOrders--;
+                        break;
+                    }
+                }
+
+                _readSemaphores.Release();
+                Monitor.Pulse(this);
+            }
+
+            return retrievedOrder;
+        }
+        
+        public void setOneCell(string newOrder)
+        {
+            _writeSemaphores.WaitOne();
+
+            lock (this)
+            {
+                while (_numberOfOrders >= MaxNumberOfOrders) // thread waits for buffer to have an empty spot fill before processing orders
+                {
+                    Monitor.Wait(this);
+                }
+
+                for (int i = 0; i < MaxNumberOfOrders; i++)
+                {
+                    if (orderBuffer[i] == "empty") // do not overwrite existing order
+                    {
+                        orderBuffer[i] = newOrder;
+                        _numberOfOrders++;
+                        break;
+                    }
+                }
+
+                _writeSemaphores.Release();
+                Monitor.Pulse(this);
+            }
+        }
+
+    }
+}
