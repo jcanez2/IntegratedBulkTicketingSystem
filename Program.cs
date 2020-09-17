6d3089e71c99f18ad069ce8ca917e2cd142cfdaf
jcanez2 <jcanez2@asu.edu>
2020-09-17 00:29:35 -0700

    Added TicketAgency class to create orders to put in the buffer. Still need to add the order object itself and change the buffer to accept this order object.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace IntegratedBulkTicketingSystem
 {
+    public delegate void EventCreateOrder();
+    
     class Program
     {
+        public static ItemBuffer TicketOrderBuffer;
+        private static bool _themeParkIsOpen = true;
+        public static bool ThemeParkIsOpen
+        {
+            get => _themeParkIsOpen;
+            set => _themeParkIsOpen = value;
+        }
+
+
+
         static void Main(string[] args)
         {
+            TicketOrderBuffer = new ItemBuffer();
+
+
             Console.WriteLine("Hello World!");
         }
     }
---
+++ TicketAgency.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace IntegratedBulkTicketingSystem
+{
+    class TicketAgency
+    {
+        public static event EventCreateOrder CreateOrder;
+        public static Random Rand = new Random();
+
+        public void StartWorkTicketAgency()
+        {
+            while (Program.ThemeParkIsOpen)
+            {
+                Thread.Sleep(Rand.Next(1500, 3000));
+                NewOrder(Thread.CurrentThread.Name);
+            }
+        }
+
+        private void NewOrder(string id)
+        {
+            int cardNumber = Rand.Next(9000, 9999);
+            int numberOfTicketsRequested = Rand.Next(10, 100);
+
+            // todo:  Create order object
+            Console.WriteLine($"Ticket Agency {id} has ordered {numberOfTicketsRequested}, at {DateTime.Now:hh:mm:ss t z}.");
+             Program.TicketOrderBuffer.setOneCell("order"); // Sends order to buffer
+             CreateOrder?.Invoke(); // Lets Theme park know an order was created
+        }
+
+        public void OrderHasBeenProcessed(int id, int totalAmount, int priceOfTickets, int numberOfTickets)
+        {
+            Console.WriteLine($"Ticket Agency {id} order has been processed, with a ticket price of ${priceOfTickets} for {numberOfTickets} tickets. The total of the transaction was {totalAmount}.");
+        }
+
+        public void TicketsAreOnSale(string id, int price)
+        {
+            Console.WriteLine($"Ticket Agency {id}: Tickets are on sale for ${price}, we are placing an order.");
+            NewOrder(id);
+        }
+    }
+}
