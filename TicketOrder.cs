ef00f608b5e8735767673caf6fb45db3fcc5a033
jcanez2 <jcanez2@asu.edu>
2020-09-17 00:50:27 -0700

    Added the TicketOrder class and test cases for the this class.

---
+++ TicketOrder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace IntegratedBulkTicketingSystem
+{
+    public class TicketOrder
+    {
+        private int _price;
+        private int _cardNumber;
+        private string _id;
+
+        public int Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+
+        public int CardNumber
+        {
+            get => _cardNumber;
+            set => _cardNumber = value;
+        }
+
+        public string Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public TicketOrder(int price, int cardNumber, string id)
+        {
+            this._price = price;
+            this._cardNumber = cardNumber;
+            this._id = id;
+        }
+
+    }
+}
